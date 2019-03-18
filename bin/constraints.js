e5c33996c62828324f3fdae10283ed5be1c79826
Bram Gotink <bram@gotink.me>
2019-03-18 22:14:11 +0100

    {fix} use path to constraint binary file instead of "node"

--- bin/constraints.js
+++ bin/constraints.js
@@ -5,4 +5,8 @@ const {join} = require('path');
 
 concierge.directory(join(__dirname, '..', 'dist', 'commands'), true, /\.js$/);
 
-concierge.runExit(process.argv0, process.argv.slice(2));
+// process.argv0 is 'node', process.argv[0] is the absolute path to the node executable
+// process.argv[1] is the actual path to the constraints.js file
+const argv0 = process.argv[1];
+
+concierge.runExit(argv0, process.argv.slice(2));
