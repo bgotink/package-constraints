d083e64a4a95189741ef4cf5c6aecac16c0f87dd
Bram Gotink <bram@gotink.me>
2019-03-31 22:50:17 +0200

    {feature} create "version" module to allow querying semver ranges

--- src/constraints/prolog-modules.ts
+++ src/constraints/prolog-modules.ts
@@ -6,10 +6,12 @@ import {
   MODULE_NAME as CONSTRAINTS_MODULE_NAME,
   registerModule as registerConstraintsModule,
 } from './prolog-modules/module-constraints';
+import {registerModule as registerVersionModule} from './prolog-modules/module-version';
 import {setWorkspaceInfo} from './prolog-modules/workspace-info';
 
 export function registerModules(session: pl.type.Session, workspaceInfo: WorkspaceInfo) {
   registerConstraintsModule();
+  registerVersionModule();
 
   session.consult(`:- use_module(library(${CONSTRAINTS_MODULE_NAME})).`);
   setWorkspaceInfo(session, workspaceInfo);
---
+++ src/constraints/prolog-modules/module-version.ts
@@ -0,0 +1,52 @@
+import * as semver from 'semver';
+import * as pl from 'tau-prolog';
+
+import {replaceGoal, termEquals} from './util';
+
+let registeredModule: pl.type.Module|null = null;
+
+export const MODULE_NAME = 'version';
+
+export function registerModule() {
+  if (registeredModule != null) {
+    return;
+  }
+
+  const {is_atom} = pl.type;
+
+  const predicates: Record<string, pl.type.Predicate> = {
+    'version_matches/2': (thread, point, atom) => {
+      const [range, version] = atom.args;
+
+      if (!is_atom(range) || !is_atom(version)) {
+        thread.throw_error(pl.error.instantiation(atom.indicator));
+        return;
+      }
+
+      if (semver.satisfies(version.id, range.id)) {
+        thread.success(point);
+      }
+    },
+
+    'version_minimum/2': (thread, point, atom) => {
+      const [range, version] = atom.args;
+
+      if (!is_atom(range)) {
+        thread.throw_error(pl.error.instantiation(atom.indicator));
+        return;
+      }
+
+      const minVersion = semver.minVersion(range.id);
+      thread.prepend([
+        replaceGoal(point, termEquals(version, minVersion.raw)),
+      ]);
+    },
+  };
+
+  const moduleExports = [
+    'version_matches/2',
+    'version_minimum/2',
+  ];
+
+  registeredModule = new pl.type.Module(MODULE_NAME, predicates, moduleExports);
+}
